c3fe4ecfc48db1be1df6c314849b138fa38578f6
dhansenaz <dhansen@allthingsdot.com>
2019-02-22 14:48:59 -0800

    comment props

--- src/index.js
+++ src/index.js
@@ -10,16 +10,21 @@ const App = () => {
         author='Sam'
         timeAgo='Today at 4:45pm' 
         content='awesome'
+        avatar={faker.image.avatar()}
         />
         <CommentDetail 
         author='Sue' 
         timeAgo='Today at 5:00pm' 
         content='are you gonna go?'
+        avatar={faker.image.avatar()}
+        
         />
         <CommentDetail 
         author='John' 
         timeAgo='Today at 2:00pm' 
         content='i dunno'
+        avatar={faker.image.avatar()}
+        
         />
         
     </div>
