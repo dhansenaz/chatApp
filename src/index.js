7d1ee63d10f588365c1d21f739eff30cc2aaac51
dhansenaz <dhansen@allthingsdot.com>
2019-02-23 08:15:10 -0800

    css styling

--- src/index.js
+++ src/index.js
@@ -1,35 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-import CommentDetail from './CommentDetail'
-import ApprovalCard from './ApprovalCard'
-
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="ui conatiner comments">
-    <ApprovalCard />
-        <CommentDetail 
-        author='Sam'
-        timeAgo='Today at 4:45pm' 
-        content='awesome'
-        avatar={faker.image.avatar()}
-        />
-        <CommentDetail 
-        author='Sue' 
-        timeAgo='Today at 5:00pm' 
-        content='are you gonna go?'
-        avatar={faker.image.avatar()}
-        
-        />
-        <CommentDetail 
-        author='John' 
-        timeAgo='Today at 2:00pm' 
-        content='i dunno'
-        avatar={faker.image.avatar()}
-        
-        />
-        
+    <div className="wrapper">
+      <div className="ui">
+        <ApprovalCard className="approval card">
+          <CommentDetail
+            author="Sam"
+            timeAgo="Today at 4:45pm"
+            content="awesome"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="Sue"
+            timeAgo="Today at 5:00pm"
+            content="are you gonna go?"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="John"
+            timeAgo="Today at 2:00pm"
+            content="i dunno"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+      </div>
     </div>
   );
 };
